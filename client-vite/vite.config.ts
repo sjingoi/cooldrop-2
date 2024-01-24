7a1699cf5d8cee8fa941831364ea03912bb38e15
Sebastian <sebi.jingoi@gmail.com>
2024-01-24 18:32:31 -0500

    Multi page vite

--- client-vite/vite.config.ts
+++ client-vite/vite.config.ts
@@ -1,7 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  // root,
   plugins: [svelte()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        test: resolve(__dirname, 'test', 'index.html'),
+      }
+    }
+  }
 })
