ec479054af28caa103e5fbf4346cc9e39772c549
Sebastian Jingoi <sebi.jingoi@gmail.com>
2024-01-24 22:27:03 -0500

    Added peer conneciton status

--- client/src/lib-ft/Peer.svelte
+++ client/src/lib-ft/Peer.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import type { FormEventHandler } from "svelte/elements";
-    import { PeerConnectionEvents, type FilePeerConnection, type PeerConnection } from "./PeerConnection";
+    import { PeerConnectionEvents, type FilePeerConnection, PeerConnection } from "./PeerConnection";
 
     export let peer: FilePeerConnection;
+    let connected: boolean = false;
 
     let progress = 0;
     peer.addEventListener(PeerConnectionEvents.FILE_PROGRESS, (updated_progress: any) => {progress = updated_progress.detail.progress});
+    peer.addEventListener(PeerConnectionEvents.OPEN, () => {connected = true})
 
     let files: FileList;
 
@@ -19,7 +21,13 @@
 <div>
     <h3>{peer.name}</h3>
     <p>{peer.uuid}</p>
-    <label for="file">Send File</label>
-    <input bind:files type="file" multiple/>
-    <progress value={progress}></progress>
+    {#if connected}
+        <label for="file">Send File</label>
+        <input bind:files type="file" multiple/>
+        <progress value={progress}></progress>
+    {:else}
+        <p>Connecting to peer...</p>
+    {/if}
+    
+    
 </div>
