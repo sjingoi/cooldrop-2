7d88376c321b610e36b9d636d6e822ded21e8e28
Sebastian <sebi.jingoi@gmail.com>
2024-05-15 20:17:19 -0400

    Centered Nameselect

--- android/app/src/main/java/com/example/cooldrop/nameselect/composables/NameSelectScreen.kt
+++ android/app/src/main/java/com/example/cooldrop/nameselect/composables/NameSelectScreen.kt
@@ -1,15 +1,26 @@
 package com.example.cooldrop.nameselect.composables
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NameSelectScreen(
     onSubmit: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    NameSelectUI(
-        onSubmit = onSubmit,
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
         modifier = modifier
-    )
+    ) {
+        NameSelectUI(
+            onSubmit = onSubmit,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+    }
 }
